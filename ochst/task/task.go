2e9528a502442ae0c5e657a7681d676460a14a09
phuongtnd1994 <phuongtnd.neu@gmail.com>
2024-08-28 00:21:22 +0700

    chap 04: before 4.6.4

--- ochst/task/task.go
+++ ochst/task/task.go
@@ -16,23 +16,15 @@ import (
 	"github.com/google/uuid"
 )
 
-type State int
-
-const (
-	Pending State = iota
-	Scheduled
-	Running
-	Completed
-	Failed
-)
-
 type Task struct {
 	ID            uuid.UUID
+	ContainerID   string
 	Name          string
 	State         State
 	Image         string
-	Memory        int
-	Disk          int
+	Cpu           float64
+	Memory        int64
+	Disk          int64
 	ExposedPort   nat.PortSet
 	PortBindings  map[string]string
 	RestartPolicy string
@@ -62,9 +54,30 @@ type Config struct {
 	RestartPolicy string
 }
 
+func NewConfig(t *Task) *Config {
+	return &Config{
+		Name:          t.Name,
+		ExposedPorts:  t.ExposedPort,
+		Image:         t.Image,
+		Cpu:           t.Cpu,
+		Memory:        t.Memory,
+		Disk:          t.Disk,
+		RestartPolicy: t.RestartPolicy,
+	}
+}
+
 type Docker struct {
 	Client *client.Client
 	Config Config
+}
+
+func NewDocker(c *Config) *Docker {
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+
+	return &Docker{
+		Client: dc,
+		Config: *c,
+	}
 }
 
 type DockerResult struct {
--- ochst/worker/worker.go
+++ ochst/worker/worker.go
@@ -3,6 +3,8 @@ package worker
 import (
 	"cube/task"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
@@ -23,10 +25,43 @@ func (w *Worker) RunTask() {
 	fmt.Println("I will start or stop a task")
 }
 
-func (w *Worker) StartTask() {
-	fmt.Println("I will start a task")
+func (w *Worker) StartTask(t task.Task) task.DockerResult {
+	t.StartTime = time.Now().UTC()
+	config := task.NewConfig(&t)
+	d := task.NewDocker(config)
+	result := d.Run()
+	if result.Error != nil {
+		log.Printf("Err running task %v: %v\n", t.ID, result.Error)
+		t.State = task.Failed
+		w.Db[t.ID] = &t
+		return result
+	}
+
+	t.ContainerID = result.ContainerId
+	t.State = task.Running
+	w.Db[t.ID] = &t
+
+	return result
 }
 
-func (w *Worker) StopTask() {
-	fmt.Println("I will stop a task")
+func (w *Worker) StopTask(t task.Task) task.DockerResult {
+	config := task.NewConfig(&t)
+	d := task.NewDocker(config)
+
+	result := d.Stop(t.ContainerID)
+	if result.Error != nil {
+		log.Printf("Error stoping container %v: %v\n", t.ContainerID, result.Error)
+	}
+
+	t.FinishTime = time.Now().UTC()
+	t.State = task.Completed
+	w.Db[t.ID] = &t
+
+	log.Printf("Stopped and removed container %v for task %v\n", t.ContainerID, t.ID)
+
+	return result
+}
+
+func (w *Worker) AddTask(t task.Task) {
+	w.Queue.Enqueue(t)
 }
